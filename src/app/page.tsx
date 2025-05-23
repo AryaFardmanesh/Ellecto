a2e03455c271c7b28fa3085bc8633a26c5ff27dc
Arya Fardmanesh <AryaFardmanesh.1383@gmail.com>
2025-05-23 17:16:00 +0330

    Add: product sliders.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,18 @@
-import { CSSProperties } from 'react';
+'use client';
+
+import Link from 'next/link';
 import Image from 'next/image';
-import { Carousel } from 'antd';
+import { CSSProperties } from 'react';
+import { Carousel, Typography, Card, Row } from 'antd';
 import Navbar from '@/components/navbar';
+import colors from '@/utils/colors';
+import './home.css';
+
+const { Title } = Typography;
 
 const stylesheet: { [key: string]: CSSProperties } = {
 	carousel: {
 		height: '500px',
-		color: '#fff',
-		lineHeight: '160px',
-		textAlign: 'center',
-		background: '#364d79',
 	},
 	containerSlider: {
 		width: '100%',
@@ -21,6 +24,27 @@ const stylesheet: { [key: string]: CSSProperties } = {
 	sliderImage: {
 		width: '100%',
 		height: '500px',
+	},
+	cardProductSlider: {
+		width: '100%',
+		overflowX: 'auto',
+		flexWrap: 'nowrap',
+		gap: '8px',
+	},
+	cardProductSliderCard: {
+		minWidth: '210px',
+		height: '200px',
+		padding: '10px'
+	},
+	cardProductSliderImage: {
+		width: '210px',
+		height: '145px',
+		borderRadius: 6,
+	},
+	cardProductSliderLink: {
+		background: colors.primary,
+		marginTop: 5,
+		color: 'white'
 	},
 };
 
@@ -103,6 +127,99 @@ export default function Home() {
 				</div>
 			</Carousel>
 		</header>
+
+		<div className="container my-4">
+			<Title level={3}>Newest Products:</Title>
+
+			<Card>
+				<Row style={ stylesheet.cardProductSlider }>
+					<Card style={ stylesheet.cardProductSliderCard } className='card-no-padding'>
+						<div className='d-flex flex-column justify-content-between'>
+							<Image
+								src='/slider/p-1.jpg'
+								alt='Product image for slider.'
+								width={ internalConfig.sliderImage.width }
+								height={ internalConfig.sliderImage.height }
+								style={ stylesheet.cardProductSliderImage }
+							/>
+							<Link href='/product?pid=1' style={ stylesheet.cardProductSliderLink } className='btn btn-sm'>See</Link>
+						</div>
+					</Card>
+					<Card style={ stylesheet.cardProductSliderCard } className='card-no-padding'>
+						<div className='d-flex flex-column justify-content-between'>
+							<Image
+								src='/slider/p-2.jpg'
+								alt='Product image for slider.'
+								width={ internalConfig.sliderImage.width }
+								height={ internalConfig.sliderImage.height }
+								style={ stylesheet.cardProductSliderImage }
+							/>
+							<Link href='/product?pid=1' style={ stylesheet.cardProductSliderLink } className='btn btn-sm'>See</Link>
+						</div>
+					</Card>
+					<Card style={ stylesheet.cardProductSliderCard } className='card-no-padding'>
+						<div className='d-flex flex-column justify-content-between'>
+							<Image
+								src='/slider/p-3.jpg'
+								alt='Product image for slider.'
+								width={ internalConfig.sliderImage.width }
+								height={ internalConfig.sliderImage.height }
+								style={ stylesheet.cardProductSliderImage }
+							/>
+							<Link href='/product?pid=1' style={ stylesheet.cardProductSliderLink } className='btn btn-sm'>See</Link>
+						</div>
+					</Card>
+					<Card style={ stylesheet.cardProductSliderCard } className='card-no-padding'>
+						<div className='d-flex flex-column justify-content-between'>
+							<Image
+								src='/slider/p-4.jpg'
+								alt='Product image for slider.'
+								width={ internalConfig.sliderImage.width }
+								height={ internalConfig.sliderImage.height }
+								style={ stylesheet.cardProductSliderImage }
+							/>
+							<Link href='/product?pid=1' style={ stylesheet.cardProductSliderLink } className='btn btn-sm'>See</Link>
+						</div>
+					</Card>
+					<Card style={ stylesheet.cardProductSliderCard } className='card-no-padding'>
+						<div className='d-flex flex-column justify-content-between'>
+							<Image
+								src='/slider/p-5.jpg'
+								alt='Product image for slider.'
+								width={ internalConfig.sliderImage.width }
+								height={ internalConfig.sliderImage.height }
+								style={ stylesheet.cardProductSliderImage }
+							/>
+							<Link href='/product?pid=1' style={ stylesheet.cardProductSliderLink } className='btn btn-sm'>See</Link>
+						</div>
+					</Card>
+					<Card style={ stylesheet.cardProductSliderCard } className='card-no-padding'>
+						<div className='d-flex flex-column justify-content-between'>
+							<Image
+								src='/slider/p-6.jpg'
+								alt='Product image for slider.'
+								width={ internalConfig.sliderImage.width }
+								height={ internalConfig.sliderImage.height }
+								style={ stylesheet.cardProductSliderImage }
+							/>
+							<Link href='/product?pid=1' style={ stylesheet.cardProductSliderLink } className='btn btn-sm'>See</Link>
+						</div>
+					</Card>
+					<Card style={ stylesheet.cardProductSliderCard } className='card-no-padding'>
+						<div className='d-flex flex-column justify-content-between'>
+							<Image
+								src='/slider/p-7.jpg'
+								alt='Product image for slider.'
+								width={ internalConfig.sliderImage.width }
+								height={ internalConfig.sliderImage.height }
+								style={ stylesheet.cardProductSliderImage }
+							/>
+							<Link href='/product?pid=1' style={ stylesheet.cardProductSliderLink } className='btn btn-sm'>See</Link>
+						</div>
+					</Card>
+				</Row>
+			</Card>
+		</div>
 	</>
 	);
 }
