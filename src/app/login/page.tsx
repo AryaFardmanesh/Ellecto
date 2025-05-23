8c6f3cc6344ac005a44419e39e86ee8fd635956e
Arya Fardmanesh <AryaFardmanesh.1383@gmail.com>
2025-05-23 12:04:31 +0330

    Add: formik and yup to login page.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -7,7 +7,6 @@ import {
 import {
 	Card,
 	Input,
-	Button
 } from 'antd';
 import {
 	UserOutlined,
@@ -15,6 +14,8 @@ import {
 	EyeOutlined,
 	EyeInvisibleOutlined,
 } from '@ant-design/icons';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
 import colors from '@/utils/colors';
 
 const stylesheet: {
@@ -27,7 +28,21 @@ const stylesheet: {
 	card: {
 		width: '380px',
 	},
+	submitBtn: {
+		width: '100%',
+		height: '32px',
+		background: colors.primary,
+		color: 'white',
+		border: 0,
+		borderRadius: '6px',
+		boxShadow: '0 2px 0 rgba(5,145,255,0.1)',
+	},
 };
+
+const formSchema = Yup.object( {
+	username: Yup.string().min( 6 ).max( 32 ),
+	password: Yup.string().min( 6 ).max( 32 ),
+} );
 
 export default function Login() {
 	const [ showPassword, setShowPassword ] = useState( false );
@@ -39,35 +54,64 @@ export default function Login() {
 	return (
 	<div className='container d-flex align-items-center justify-content-center' style={ stylesheet.container }>
 		<Card title='Ellecto Login' variant='borderless' style={ stylesheet.card }>
-			<form autoComplete='off'>
-				<div className='form-group'>
-					<label htmlFor='username-input'>Enter your username:</label>
-					<Input
-						placeholder='Enter username'
-						prefix={ <UserOutlined /> }
-						id='username-input'
-					/>
-					{/* <small style={{ color: colors.danger }}>We'll never share your email with anyone else.</small> */}
-				</div>
-				<div className='form-group mt-3'>
-					<label htmlFor='password-input'>Enter your password:</label>
-					<Input
-						type={ showPassword ? 'text' : 'password' }
-						placeholder='Enter password'
-						prefix={ <KeyOutlined /> }
-						suffix={
-							showPassword ? (
-								<EyeInvisibleOutlined onClick={togglePasswordIcon} />
-							) : (
-								<EyeOutlined onClick={togglePasswordIcon} />
-							)
-						}
-						id='password-input'
-					/>
-					{/* <small style={{ color: colors.danger }}>We'll never share your email with anyone else.</small> */}
-				</div>
-				<Button htmlType='button' type='primary' style={{ background: colors.primary }} className='mt-3' block>Login</Button>
-			</form>
+			<Formik
+				initialValues={{
+					username: '',
+					password: ''
+				}}
+				validationSchema={ formSchema }
+				onSubmit={ ( values, action ) => {
+					console.log( values );
+					action.resetForm();
+				} }
+			>
+				{ ( {
+					touched,
+					errors,
+					values,
+					handleSubmit,
+					handleChange,
+					handleBlur
+				} ) => (
+					<form onSubmit={ handleSubmit } autoComplete='off' spellCheck='false'>
+					<div className='form-group'>
+						<label htmlFor='username-input'>Enter your username:</label>
+						<Input
+							placeholder='Enter username'
+							prefix={ <UserOutlined /> }
+							id='username-input'
+							name='username'
+							onChange={ handleChange }
+							onBlur={ handleBlur }
+							value={ values.username }
+						/>
+						{ touched.username ? <small style={{ color: colors.danger }}>{ errors.username }</small> : null }
+					</div>
+					<div className='form-group mt-3'>
+						<label htmlFor='password-input'>Enter your password:</label>
+						<Input
+							type={ showPassword ? 'text' : 'password' }
+							placeholder='Enter password'
+							prefix={ <KeyOutlined /> }
+							suffix={
+								showPassword ? (
+									<EyeInvisibleOutlined onClick={togglePasswordIcon} />
+								) : (
+									<EyeOutlined onClick={togglePasswordIcon} />
+								)
+							}
+							id='password-input'
+							name='password'
+							onChange={ handleChange }
+							onBlur={ handleBlur }
+							value={ values.password }
+						/>
+						{ touched.password ? <small style={{ color: colors.danger }}>{ errors.password }</small> : null }
+					</div>
+					<button style={ stylesheet.submitBtn } className='mt-3'>Login</button>
+					</form>
+				) }
+				</Formik>
 		</Card>
 	</div>
 	);
