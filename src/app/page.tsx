fad490a153d383ce4766ad64bfdadf8ae0f7d62f
Arya Fardmanesh <AryaFardmanesh.1383@gmail.com>
2025-05-23 16:29:03 +0330

    Add: header slider.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,101 @@
-import Navbar from "@/components/navbar";
+import { CSSProperties } from 'react';
+import Image from 'next/image';
+import { Carousel } from 'antd';
+import Navbar from '@/components/navbar';
+
+const stylesheet: { [key: string]: CSSProperties } = {
+	carousel: {
+		height: '500px',
+		color: '#fff',
+		lineHeight: '160px',
+		textAlign: 'center',
+		background: '#364d79',
+	},
+	containerSlider: {
+		width: '100%',
+		height: '100%',
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	sliderImage: {
+		width: '100%',
+		height: '500px',
+	},
+};
 
 export default function Home() {
 	return (
 	<>
 		<Navbar />
-		<h1>Hello world!</h1>
+
+		<header>
+			<Carousel arrows autoplay={{ dotDuration: true }} autoplaySpeed={ 4000 } style={ stylesheet.carousel }>
+				<div style={ stylesheet.containerSlider }>
+					<Image
+						src='/slider/p-1.jpg'
+						alt='Product image for slider.'
+						width={ 2000 }
+						height={ 500 }
+						style={ stylesheet.sliderImage }
+					/>
+				</div>
+				<div style={ stylesheet.containerSlider }>
+					<Image
+						src='/slider/p-2.jpg'
+						alt='Product image for slider.'
+						width={ 2000 }
+						height={ 500 }
+						style={ stylesheet.sliderImage }
+					/>
+				</div>
+				<div style={ stylesheet.containerSlider }>
+					<Image
+						src='/slider/p-3.jpg'
+						alt='Product image for slider.'
+						width={ 2000 }
+						height={ 500 }
+						style={ stylesheet.sliderImage }
+					/>
+				</div>
+				<div style={ stylesheet.containerSlider }>
+					<Image
+						src='/slider/p-4.jpg'
+						alt='Product image for slider.'
+						width={ 2000 }
+						height={ 500 }
+						style={ stylesheet.sliderImage }
+					/>
+				</div>
+				<div style={ stylesheet.containerSlider }>
+					<Image
+						src='/slider/p-5.jpg'
+						alt='Product image for slider.'
+						width={ 2000 }
+						height={ 500 }
+						style={ stylesheet.sliderImage }
+					/>
+				</div>
+				<div style={ stylesheet.containerSlider }>
+					<Image
+						src='/slider/p-6.jpg'
+						alt='Product image for slider.'
+						width={ 2000 }
+						height={ 500 }
+						style={ stylesheet.sliderImage }
+					/>
+				</div>
+				<div style={ stylesheet.containerSlider }>
+					<Image
+						src='/slider/p-7.jpg'
+						alt='Product image for slider.'
+						width={ 2000 }
+						height={ 500 }
+						style={ stylesheet.sliderImage }
+					/>
+				</div>
+			</Carousel>
+		</header>
 	</>
 	);
 }
